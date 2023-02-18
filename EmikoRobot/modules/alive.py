11fc2e2b2b1caefb321bb64ff30cee77bc42c241
lexaamd <amandeshere@gmail.com>
2023-02-19 02:10:18 +0700

    AmdRobot

---
+++ EmikoRobot/modules/alive.py
@@ -0,0 +1,24 @@
+import os
+import re
+from platform import python_version as kontol
+from telethon import events, Button
+from telegram import __version__ as telever
+from telethon import __version__ as tlhver
+from pyrogram import __version__ as pyrover
+from EmikoRobot.events import register
+from EmikoRobot import telethn as tbot
+
+
+PHOTO = "https://telegra.ph/file/ddad9de7cf0f5a6ec125c.jpg"
+
+@register(pattern=("/alive"))
+async def awake(event):
+  TEXT = f"**Hi [{event.sender.first_name}](tg://user?id={event.sender.id}), Halo tod, Gua Amd Robot.** \n\n"
+  TEXT += "⚪ **I'm Working Properly** \n\n"
+  TEXT += f"⚪ **My Master : [Lexa] (https//t.me/Lexanih)** \n\n"
+  TEXT += f"⚪ **Library Version :** `{telever}` \n\n"
+  TEXT += f"⚪ **Telethon Version :** `{tlhver}` \n\n"
+  TEXT += f"⚪ **Pyrogram Version :** `{pyrover}` \n\n"
+  TEXT += "**Thanks For Adding Me Here ❤️**"
+  BUTTON = [[Button.url("Help", "https://t.me/Lexanih?start=help"), Button.url("Support", "https://t.me/Amandes_nih")]]
+  await tbot.send_file(event.chat_id, PHOTO, caption=TEXT,  buttons=BUTTON)
